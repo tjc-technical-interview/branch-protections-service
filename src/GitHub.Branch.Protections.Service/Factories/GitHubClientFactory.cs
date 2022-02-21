ba7c763f38e75f3e3bfd87aa94b6461bbe3d9b12
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2022-02-21 12:58:18 -0500

    Added GitHub client factory

---
+++ src/GitHub.Branch.Protections.Service/Factories/GitHubClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using Octokit;
+
+namespace GitHub.Branch.Protections.Service.Factories
+{
+    public static class GitHubClientFactory
+    {
+        public static IGitHubClient CreateGitHubClient(string token)
+        {
+            var tokenAuth = new Credentials(token);
+
+            var client = new GitHubClient(new ProductHeaderValue("github-branch-protections"))
+            {
+                Credentials = tokenAuth
+            };
+
+            return client;
+        }
+    }
+}
