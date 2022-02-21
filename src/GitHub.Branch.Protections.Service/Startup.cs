eb005653bc87fabc3c7f328597e084773c8d0402
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2022-02-21 12:59:20 -0500

    Added GitHub webhook handles
    Added hook for ping test
    Added hook for branch protection rules
    Added hook for repo pushes
    Added logic to enforce minimum branch protection rules

---
+++ src/GitHub.Branch.Protections.Service/Startup.cs
@@ -0,0 +1,31 @@
+﻿using GitHub.Branch.Protections.Service.Factories;
+using Octokit;
+using Octokit.Webhooks;
+using Octokit.Webhooks.AspNetCore;
+
+namespace GitHub.Branch.Protections.Service;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => { endpoints.MapGitHubWebhooks(secret: "some random secret"); });
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var token = Configuration["token"];
+
+        services.AddSingleton<IGitHubClient>(GitHubClientFactory.CreateGitHubClient(token));
+        services.AddSingleton<WebhookEventProcessor, GitHubEvent>();
+    }
+}
